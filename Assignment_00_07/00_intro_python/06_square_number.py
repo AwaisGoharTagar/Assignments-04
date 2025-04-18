2cb95d9cf40594c2540d9fe52d01cd96ae5dec1f
AwaisGoharTagar <awaisgohar4560@gmail.com>
2025-04-18 17:58:06 +0500

    first commit

---
+++ Assignment_00_07/00_intro_python/06_square_number.py
@@ -0,0 +1,13 @@
+def main():
+
+    number = float(input(" \033[1;3m Type a number to see its square: \033[0m "))
+    
+    # Calculate the square of the number
+    square = number * number
+    
+    print(f" The square of {number} is {square}")
+
+
+
+if __name__ == '__main__':
+    main()
